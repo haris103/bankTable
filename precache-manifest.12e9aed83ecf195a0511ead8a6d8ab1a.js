9cd57ce181dabda9927fc8aafade5b4778981fbd
Haris Mahmood <haris.mahmood103@gmail.com>
2022-04-14 23:55:54 +0500

    Updates

---
+++ precache-manifest.12e9aed83ecf195a0511ead8a6d8ab1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a581d9aebd132cc47146",
+    "url": "/bankTable/static/css/main.1d8d263a.chunk.css"
+  },
+  {
+    "revision": "a581d9aebd132cc47146",
+    "url": "/bankTable/static/js/main.a581d9ae.chunk.js"
+  },
+  {
+    "revision": "aef5be1ab7919be08f37",
+    "url": "/bankTable/static/js/1.aef5be1a.chunk.js"
+  },
+  {
+    "revision": "907c24682c6fa104ea09",
+    "url": "/bankTable/static/js/2.907c2468.chunk.js"
+  },
+  {
+    "revision": "d7b350f5a7248e31f66d",
+    "url": "/bankTable/static/js/runtime~main.d7b350f5.js"
+  },
+  {
+    "revision": "969cdd918743cfbb41341e4b8f167167",
+    "url": "/bankTable/index.html"
+  }
+];
